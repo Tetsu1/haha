d9440460d114236b0db9e2400eca9cc5b58f98ae
Hazem Ashraf <tkuroko121@gmail.com>
2016-06-22 16:29:58 +0300

    Upload To Github

--- js/plugin.js
+++ js/plugin.js
@@ -1,9 +1,21 @@
-/*global $, console, jquery, alert*/
+/*global console, alert, $*/
+/*jslint plusplus: true, evil: true*/
 
-$(function () {
-   
-    "use strict";
+var div = document.createElement("div");
 
-    
-    
-});
+div.id = "test";
+
+div.style.backgroundColor = "#00BCD4";
+div.style.width = "50px";
+div.style.height = "50px";
+div.style.borderRadius = "50%";
+div.style.position = "relative";
+div.style.left = "20px";
+
+document.body.appendChild(div);
+
+var myDiv = document.getElementById("test");
+
+function screenClick(e) {
+    myDiv.
+}d
